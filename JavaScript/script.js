d0b6d7ab4b15e1bd650a15863662a5f8bd886daa
Nah <nguyenbinhan2707@gmail.com>
2025-05-18 00:23:13 +0700

    Dashboard Editor, Edit post

---
+++ JavaScript/script.js
@@ -0,0 +1,33 @@
+// hieu ung button menu
+const btnOpen = document.getElementById('btn-open');
+  const sidebar = document.querySelector('.sidebar');
+  const mainContent = document.querySelector('.main-content');
+ const btnIcon = btnOpen.querySelector('i');
+
+  btnOpen.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+    mainContent.classList.toggle('expanded');
+    btnOpen.classList.toggle('rotated'); 
+
+    //  // Đổi icon khi mở sidebar
+    // if (sidebar.classList.contains('collapsed')) {
+    //   btnIcon.classList.remove('fa-bars');
+    //   btnIcon.classList.add('fa-xmark');
+    // } else {
+    //   btnIcon.classList.remove('fa-xmark');
+    //   btnIcon.classList.add('fa-bars');
+    // }
+  });
+
+  document.getElementById("create-button").addEventListener("click", function () {
+    alert("Bài viết đã được tạo thành công!");
+});
+
+document.getElementById("cancel-button").addEventListener("click", function () {
+    document.getElementById("post-title").value = "";
+    document.getElementById("post-content").value = "";
+    document.getElementById("image-upload").value = "";
+});
+
+
+  
