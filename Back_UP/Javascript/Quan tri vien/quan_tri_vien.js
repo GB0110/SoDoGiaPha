cb1973d20155a08ecc5bd37f5203b945093c490e
hachieu111 <hachieu375@gmail.com>
2025-05-22 20:52:50 +0700

    cmt và add xem trước ảnh

--- Back_UP/Javascript/Quan tri vien/quan_tri_vien.js
+++ Back_UP/Javascript/Quan tri vien/quan_tri_vien.js
@@ -211,3 +211,17 @@ window.confirmDelete = async function () {
   closePopup("popupDelete");
   await loadUsersFromFirestore();
 };
+// xem trước ảnh
+window.previewAvatar = function(event) {
+  const file = event.target.files[0];
+  const preview = document.getElementById("avatarPreview");
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      preview.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "avatar-default.png";
+  }
+};
